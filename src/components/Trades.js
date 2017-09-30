bdc39633b778ceca24790aae63cc786632dc9f71
brentkirkland <brentckirkland@gmail.com>
2017-09-30 14:41:57 -0700

    fixed time bug

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -5,7 +5,7 @@ import './App.css';
 class Trades extends Component {
 
   convertToNiceTime (unix_timestamp) {
-    const date = new Date(unix_timestamp*1000);
+    const date = new Date(unix_timestamp);
     const hours = date.getHours();
     const minutes = "0" + date.getMinutes();
     const seconds = "0" + date.getSeconds();
